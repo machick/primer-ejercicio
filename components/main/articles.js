9d2c9f9862345fec1ee9ba9c41e3979fdd14d725
Machi <josemiguelmc_1998@hotmail.com>
2022-07-19 01:03:35 -0300

    fix idioma fecha

--- components/main/articles.js
+++ components/main/articles.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import moment from 'moment';
+import 'moment/locale/es';
 import { useArticlesContext } from "../../context/context";
 
 export default function Articles() {
