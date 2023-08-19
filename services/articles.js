26dc9da1722f006fb58b3856426a2c7c44899626
Machi <josemiguelmc_1998@hotmail.com>
2023-08-19 15:45:04 -0300

    update url api

--- services/articles.js
+++ services/articles.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export default async function getArticlesFromApi() {
-     const resp = await Axios.get('https://api-test-ln.herokuapp.com/articles')
+     const resp = await Axios.get('https://jrt2bb3b2nlkw5ozvfcld62wbe0pnifh.lambda-url.us-east-1.on.aws/')
      .catch((error) => {
         console.error(error.message);
      });
